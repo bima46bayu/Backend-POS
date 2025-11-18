495f86e1e962959cc4b5d6cd2509ed8f74ae2972
BIMA <bima46bayu25@gmail..com>
2025-11-18 09:21:57 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,21 @@
 <?php
 
 return [
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
-        'https://peaceful-williams.202-10-44-26.plesk.page',
+        env('FRONTEND_URL', 'https://peaceful-williams.202-10-44-26.plesk.page'),
     ],
-
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Accept',
+        'Origin',
+        'Authorization',
+    ],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    // true kalau kamu pakai cookie / Sanctum
-    // kalau cuma pakai Bearer token, boleh false
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
+
