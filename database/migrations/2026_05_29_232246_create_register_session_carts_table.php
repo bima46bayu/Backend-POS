ec72827f066bebfffeb222dae11fee9ae84fc953
BIMA <bima46bayu25@gmail..com>
2026-05-29 23:30:59 +0700

    merge: cart feature

---
+++ database/migrations/2026_05_29_232246_create_register_session_carts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('register_session_carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('register_session_id')
+                  ->unique()
+                  ->constrained('register_sessions')
+                  ->cascadeOnDelete();
+            $table->json('items')->nullable();
+            $table->json('checkout')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('register_session_carts');
+    }
+};
